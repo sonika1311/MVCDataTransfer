505e1b10738002bdc39e6fb1db529ed07f96578e
sonika1311 <sonikakumaribom@gmail.com>
2024-06-24 15:02:44 +0530

    Added examples for data transfer via ViewData.

--- MVCDataTransfer/App_Start/RouteConfig.cs
+++ MVCDataTransfer/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace MVCDataTransfer
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{id}/{name}/{price}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, name = UrlParameter.Optional, price = UrlParameter.Optional }
             );
         }
     }
---
+++ MVCDataTransfer/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDataTransfer.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index1(int? id, string name, double? price)
+        {
+            ViewData["Id"] = id;
+            ViewData["Name"] = name;
+            ViewData["Price"] = price;
+            return View();
+        }
+        public ViewResult Display1()
+        {
+            List<string> Colors = new List<string>() { "Red", "Blue", "Pink", "Black", "White", "Green", "Brown", "Purple" };
+            ViewData["Colors"] = Colors;
+            return View();
+        }
+    }
+}
