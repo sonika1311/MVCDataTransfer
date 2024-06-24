8f408ce45f0b3a120ff9ede1556b0e9923e59f24
sonika1311 <sonikakumaribom@gmail.com>
2024-06-24 16:55:01 +0530

    Added changes to test/demonstrate TempData for data transfer from action method to controller. ALso used Keep and Peek methods to retain the data after accessing.

---
+++ MVCDataTransfer/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDataTransfer.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ViewResult Index1()
+        {
+            return View();
+        }
+        public ViewResult Display1()
+        {
+            return View();
+        }
+    }
+}
